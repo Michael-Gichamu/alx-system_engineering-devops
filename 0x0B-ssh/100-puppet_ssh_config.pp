47a6c86bc7843239f801cdac390cbbd8de2d4aed
Michael-Gichamu <michaelmatere606@gmail.com>
2023-06-24 08:32:37 +0000

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Client configuration file (w/ Puppet)
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/sshd_config',
+  line => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile',
+}
