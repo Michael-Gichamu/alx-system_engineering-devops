f4cc8e6a632e872ae1f4447a3c8de355b0419f40
Michael-Gichamu <michaelmatere606@gmail.com>
2023-07-24 16:37:26 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,32 +2,38 @@
 import requests
 import sys
 
-employeeid = int(sys.argv[1])
-total_task = 0
-done_task = 0
 
-namereq = requests.get('https://jsonplaceholder.typicode.com/users')
-usersdict = namereq.json()
+def main():
+    employeeid = int(sys.argv[1])
+    total_task = 0
+    done_task = 0
 
-for user in usersdict:
-    if user['id'] == employeeid:
-        name = user['name']
+    namereq = requests.get('https://jsonplaceholder.typicode.com/users')
+    usersdict = namereq.json()
 
-todoreq = requests.get('https://jsonplaceholder.typicode.com/todos')
-tododict = todoreq.json()
-users = []
+    for user in usersdict:
+        if user['id'] == employeeid:
+            name = user['name']
 
-for user in tododict:
-    if user['userId'] == 1:
-        users.append(user)
+    todoreq = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tododict = todoreq.json()
+    users = []
 
-for user in users:
-    total_task += 1
-    if user['completed'] == True:
-        done_task += 1
+    for user in tododict:
+        if user['userId'] == employeeid:
+            users.append(user)
 
-print(f"Employee {name} is done with tasks({done_task}/{total_task}):")
-for user in users:
-    if user['completed'] == True:
-        title = user['title']
-        print(f"\t {title}")
+    for user in users:
+        total_task += 1
+        if user['completed'] is True:
+            done_task += 1
+
+    print(f"Employee {name} is done with tasks({done_task}/{total_task}):")
+    for user in users:
+        if user['completed'] is True:
+            title = user['title']
+            print(f"\t {title}")
+
+
+if __name__ == "__main__":
+    main()
