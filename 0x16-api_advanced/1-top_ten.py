2e77d02da6f150d9c382532b6f03ffa9dbb5f525
Michael-Gichamu <michaelmatere606@gmail.com>
2023-08-08 12:39:22 +0000

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Function that Queries Reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts
+    listed for a given subreddit
+    """
+    if subreddit is None or type(subreddit) is not str:
+        return None
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    user_agent = '0x16-api_advanced v1.0 (by /u/Droguemike)'
+    headers = {'User-Agent': user_agent}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for index, post in enumerate(posts):
+            print(f"{post['data']['title']}")
+    else:
+        return None
